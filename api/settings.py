08130522046f5830c37d666e74f72d41d8af9216
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-04 11:47:45 +0100

    added allowed host

--- api/settings.py
+++ api/settings.py
@@ -68,12 +68,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # ::::: MYSQL
 # DATABASES = {
 #     'default': {
@@ -86,16 +86,16 @@ WSGI_APPLICATION = 'api.wsgi.application'
 #     }
 # }
 # :::PSOTGRESS
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'todopostdb',
-        'USER':'postgres',
-        'PASSWORD': str(os.getenv('PASSWORD')),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'todopostdb',
+#         'USER':'postgres',
+#         'PASSWORD': str(os.getenv('PASSWORD')),
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
