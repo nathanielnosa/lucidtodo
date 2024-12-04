a9f19543a1a9ec1c987777bed90341c33ce998fb
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-04 11:43:53 +0100

    added allowed host

--- api/settings.py
+++ api/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lucidtodo.onrender.com','localhost:8000','127.0.0.1']
 
 
 # Application definition
