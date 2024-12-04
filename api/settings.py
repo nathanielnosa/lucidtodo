bf1edc10275a517a1479303e4ba6d1eea5506b8c
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-04 12:19:38 +0100

    connect to render

--- api/settings.py
+++ api/settings.py
@@ -152,4 +152,4 @@ CORS_ORIGIN_WHITELIST=[
     'http://127.0.0.1:5500',
     'http://localhost:5500'
 ]
-CSRF_TRUSTED_ORIGINS = ['https://lucidtodo.onrender.com','lucidtodo.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://lucidtodo.onrender.com']
